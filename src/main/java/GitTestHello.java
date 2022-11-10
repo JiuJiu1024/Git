4e6105bd006720538c12d2b650e12d0335327d9f
JiuJiu <101798427+JiuJiu1024@users.noreply.github.com>
2022-11-10 17:55:48 +0800

    Update GitTestHello.java

--- src/main/java/GitTestHello.java
+++ src/main/java/GitTestHello.java
@@ -1,5 +1,6 @@
 public class GitTestHello {
     public static void main(String[] args) {
         System.out.println("Hello Github Test One");
+        System.out,println("Hello IDEA Test One")
     }
 }
